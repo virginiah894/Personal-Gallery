434aaf889bf02541b800b7c590b196ec1c615585
virginiah894 <virgyperry@gmail.com>
2019-12-14 18:15:48 +0300

    setup tests for location and category

--- gallery/models.py
+++ gallery/models.py
@@ -3,4 +3,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+  types = models.CharField(max_length =100,null=True )
+  def __str__(self):
+    return self.types
+
+class Location(models.Model):
+  name = models.CharField(max_length =100,null=True)
+  
+  def __str__(self):
+    return self.name
+
+class Image(models.Model):
+  Name = models.CharField(max_length = 40,null=True)
+  Description = models.TextField()
+  location = models.ForeignKey(Location,null=True)
+  category = models.ForeignKey(Category,null=True)
+  # Image = models.ImageField(upload_to='photos/')
--- gallery/tests.py
+++ gallery/tests.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from gallery.models import Image,Category,Location
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+
+  def setUp(self):
+
+    self.cat1 = Category(types='food')
+  def test_instance(self):
+    self.assertTrue(isinstance(self.cat1,Category))
+class LocationTestClass(TestCase):
+  def setUp(self):
+
+    self.name = Location(name='kenya')
+  def test_instance(self):
+    self.assertTrue(isinstance(self.name,Location))
