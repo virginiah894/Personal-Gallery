688be3310dc12b66c0a491f2f36862fb234b8283
virginiah894 <virgyperry@gmail.com>
2019-12-14 13:30:52 +0300

    add routes to display images of different days

--- gallery/views.py
+++ gallery/views.py
@@ -2,6 +2,28 @@
 # from __future__ import unicode_literals
 
 from django.shortcuts import render
+import datetime as dt
+
 
 def home(request):
     return render(request,'home.html')
+
+def images_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-images/today-images.html', {"date": date,})
+
+def past_days_images(request, past_date):
+
+    try:
+        
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(images_of_day)
+
+    return render(request, 'all-images/past-images.html', {"date": date})
