2099af8f111e3d9aa713ea4c95c310b5cd854324
virginiah894 <virgyperry@gmail.com>
2019-12-16 15:17:21 +0300

    update procfile

--- virginiah/settings.py
+++ virginiah/settings.py
@@ -53,7 +53,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 't&emvln0e_62hr#5z69wg33-f3n+=)cyd-+$^7z*5bkvr$$^r6'
+SECRET_KEY = 't&emvln0e_62hr#5z69wg33-f3n+=)cyd-+$^7z*5bkvr$$^r6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
